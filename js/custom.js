9c9053bc82c2da0c80e9c5506fa9ba050a6751f7
Ahmed-joy <113248394+Ahmed-joy@users.noreply.github.com>
2022-11-16 18:19:41 -0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(function(){
+    // member part slider//
+   
+  
+    $('.slider').slick({
+        arrows: true,
+        prevArrow:'<i class="fa-solid fa-angle-right"></i>',
+        nextArrow:'<i class="fa-solid fa-angle-left"></i>',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      });
+})
+          
